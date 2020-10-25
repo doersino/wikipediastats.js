e9e98008d3af98b2403ca045ef755329a1df560e
Noah Doersing <git@noahdoersing.com>
2020-10-25 20:37:11 +0100

    Third in-progress checkin

--- index.js
+++ index.js
@@ -17,71 +17,116 @@ const cheerio = require('cheerio')
 
 const Twitter = require('twitter')
 
-// TODO use class syntactical sugar instead
-function Logger (v0) {
-  this.v0 = parseInt(v0)
+class Logger {
+  constructor(v0) {
+    this.v0 = parseInt(v0)
+  }
 
-  this.log = (v, message) => {
-    if (this.v0 >= v) {
-      // TODO split into lines first?
-      console.log(message)
+  #timestampify(message) {
+
+    // stringify
+    message = "" + message
+
+    // add timestamp and pad everything but the first line accordingly
+    const timestamp = new Date().toISOString() +  " - "
+    const lines = message.split("\n")
+    let first = lines[0]
+    let rest = ""
+    if (lines.length > 1) {
+      first += "\n"
+      rest = lines.slice(1).map(l => "".padStart(timestamp.length, ' ') + l).join("\n")
+    }
+
+    return timestamp + first + rest
+  }
+
+  #log(message) {
+    console.log(this.#timestampify(message))
+  }
+
+  #err(message) {
+    console.error(this.#timestampify(message))
+  }
+
+  debug(object) {
+    if (this.v0 >= 2) {
+      this.#log(JSON.stringify(object))
     }
   }
 
-  this.debug = message => { this.log(2, message) }
-  this.status = message => { this.log(1, message) }
-  this.error = message => { this.log(0, 'error: ' + message) } // TODO console.error?
+  status(message) {
+    if (this.v0 >= 1) {
+      this.#log(message)
+    }
+  }
+
+  error(message) {
+    if (this.v0 >= 0) {
+      if (message.hasOwnProperty("stack")) {
+        this.#err("A bad thing has happened:\n" + message.stack)
+      } else {
+        this.#err("A bad thing has happened:\n" + message)
+      }
+      process.exit(1)
+    }
+  }
 }
 
-logger = new Logger(config.verbosity)
-logger.status('test')
-process.on('uncaughtException', function (err) {
-  logger.error(err.stack)
-  // TODO exit?
-})
+// request list of wikipedias
+function getWikipedias() {
+  const wikipediasTableURL = 'https://meta.wikimedia.org/wiki/List_of_Wikipedias/Table'
+
+  return axios.get(wikipediasTableURL)
+    .then(src => {
+      // parse html code
+      const $ = cheerio.load(src.data)
+
+      // extract all table rows
+      const rows = $('.mw-parser-output table tr')
+
+      let languages = []
+      rows.each((_, e) => {
+        // ignore table headings (which contain th elements instead of td)
+        if ($(e).find('th').length > 0) {
+          return
+        }
+
+        // extract only table cells from current row (i.e. drop text nodes in-
+        // between which basically only contain whitespace anyway)
+        const cells = $(e).find('td')
+
+        // for each row, the second cell (=> index 1) contains the language name
+        // (e.g. "English"), and the fourth (=> 3) contains the subdomain (e.g.
+        // "en")
+        const language = $(cells[1]).text().trim()
+        const subdomain = $(cells[3]).text().trim()
+
+        // accumulate them
+        languages.push({ subdomain: subdomain, language: language })
+      })
+
+      // drop the last row's contribution (it stems from a summary table at the
+      // bottom of the page)
+      languages = languages.slice(0, -1)
+
+      return languages
+    })
+}
+
+function getStats(w) {
+  const wikipediaStatsURL = 'https://' + w.subdomain + '.wikipedia.org/wiki/Special:Statistics'
+  return axios.get(wikipediaStatsURL)
+    .then(src => {
+      // parse html code
+      const $ = cheerio.load(src.data)
+
+      // TODO do things here
+    })
+}
 
 // TODO interact with cache
 // config.get('cacheFile')
 
-const wikipediasTableURL = 'https://meta.wikimedia.org/wiki/List_of_Wikipedias/Table'
-const wikipediaStatsURL = subdomain => 'https://' + subdomain + '.wikipedia.org/wiki/Special:Statistics'
-
-// TODO move this into a function getWikipedias() that can be called in a promise-y way, with await???
-// request list of wikipedias
-axios.get('https://meta.wikimedia.org/wiki/List_of_Wikipedias/Table')
-  .then(src => {
-    // parse html code
-    const $ = cheerio.load(src.data)
-
-    // extract all table rows
-    const rows = $('.mw-parser-output table tr')
-
-    let languages = []
-    rows.each((_, e) => {
-      // ignore table headings (which contain th elements instead of td)
-      if ($(e).find('th').length > 0) {
-        return
-      }
-
-      // extract only table cells from current row (i.e. drop text nodes in-
-      // between which basically only contain whitespace anyway)
-      const cells = $(e).find('td')
-
-      // for each row, the second cell (=> index 1) contains the language name
-      // (e.g. "English"), and the fourth (=> 3) contains the subdomain (e.g.
-      // "en")
-      const language = $(cells[1]).text().trim()
-      const subdomain = $(cells[3]).text().trim()
-
-      // accumulate them
-      languages.push({ subdomain: subdomain, language: language })
-    })
-
-    // drop the last row's contribution (it stems from a summary table at the
-    // bottom of the page)
-    languages = languages.slice(0, -1)
-  })
-  .catch(err => console.log(err))
 
 // TODO https://www.npmjs.com/package/twitter
 /* const client = new Twitter({
@@ -105,7 +150,23 @@ client.post('statuses/update', { status: 'testing' })
 // via https://stackoverflow.com/a/54763194
 
 async function run() {
-  //const wikipedias = await asyncTask().catch(error => console.error(error));
+  logger = new Logger(config.verbosity)
+  logger.status("Successfully loaded configuration.")
+
+  logger.status("Getting list of Wikipedias... ")
+  const wikipedias = await getWikipedias().catch(error => logger.error(error));
+  logger.debug(wikipedias)
+
+  const m = wikipedias.length
+  let n = 1
+  wikipedias.forEach(async wiki => {
+    // TODO hmm, make the status thingy less async, somehow? idk?
+    logger.status("Getting stats for " + wiki.subdomain + ".wikipedia.org (" + n + "/" + m + ")... ")
+    n++
+    const stats = await getStats(wiki).catch(error => logger.error(error));
+    // TODO more
+    // TODO instead of erroring, maybe just emit a warning and carry on without this wikipedia? but do emit an error if there are more than a few errors? idk
+  })
 
 
 
@@ -121,4 +182,7 @@ async function run() {
   compare and construct tweets and send em
   then
   write new cache to disk
+  */
 }
+
+run()
