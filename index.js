4b1d5cf091ffb6af8e6d90de3cdb76618c65ef18
Noah Doersing <git@noahdoersing.com>
2020-10-25 19:48:47 +0100

    Another in-progress checkin

--- index.js
+++ index.js
@@ -3,101 +3,98 @@
 
 // TODO make the thing
 
+// this automatically loads "config/default.yaml" if existant in the current working directory
+// TODO or the one this file is located in, via: process.env["NODE_CONFIG_DIR"] = __dirname + "/configDir/";
+// TODO config, see: https://github.com/lorenwest/node-config/wiki/Configuration-Files
 const config = require('config')
 
-const fs = require('fs')  // TODO file system interaction
+const fs = require('fs') // TODO file system interaction
 
-const axios = require('axios') // i would be using the built-in https module, but it doesn't provide a promise-based interface, unlike the built-in http module, which makes zero sense, which is why i'm using axios
+// i would be using the built-in https module, but it doesn't provide a promise-based interface, unlike the built-in http module, which makes zero sense, which is why i'm using axios
+const axios = require('axios')
 
-const jsdom = require('jsdom')
-const { JSDOM } = jsdom
+const cheerio = require('cheerio')
 
 const Twitter = require('twitter')
 
-// TODO do this in ts? or might that be too much for this first project?
-// TODO config, see: https://github.com/lorenwest/node-config/wiki/Configuration-Files
+// TODO use class syntactical sugar instead
+function Logger (v0) {
+  this.v0 = parseInt(v0)
+
+  this.log = (v, message) => {
+    if (this.v0 >= v) {
+      // TODO split into lines first?
+      console.log(message)
+    }
+  }
+
+  this.debug = message => { this.log(2, message) }
+  this.status = message => { this.log(1, message) }
+  this.error = message => { this.log(0, 'error: ' + message) } // TODO console.error?
+}
+
+logger = new Logger(config.verbosity)
+logger.status('test')
+process.on('uncaughtException', function (err) {
+  logger.error(err.stack)
+  // TODO exit?
+})
+
+// TODO interact with cache
+// config.get('cacheFile')
+
+const wikipediasTableURL = 'https://meta.wikimedia.org/wiki/List_of_Wikipedias/Table'
+const wikipediaStatsURL = subdomain => 'https://' + subdomain + '.wikipedia.org/wiki/Special:Statistics'
+
+// TODO move this into a function getWikipedias() that can be called in a promise-y way, with await???
+// request list of wikipedias
+axios.get('https://meta.wikimedia.org/wiki/List_of_Wikipedias/Table')
+  .then(src => {
+    // parse html code
+    const $ = cheerio.load(src.data)
+
+    // extract all table rows
+    const rows = $('.mw-parser-output table tr')
+
+    let languages = []
+    rows.each((_, e) => {
+      // ignore table headings (which contain th elements instead of td)
+      if ($(e).find('th').length > 0) {
+        return
+      }
+
+      // extract only table cells from current row (i.e. drop text nodes in-
+      // between which basically only contain whitespace anyway)
+      const cells = $(e).find('td')
+
+      // for each row, the second cell (=> index 1) contains the language name
+      // (e.g. "English"), and the fourth (=> 3) contains the subdomain (e.g.
+      // "en")
+      const language = $(cells[1]).text().trim()
+      const subdomain = $(cells[3]).text().trim()
+
+      // accumulate them
+      languages.push({ subdomain: subdomain, language: language })
+    })
+
+    // drop the last row's contribution (it stems from a summary table at the
+    // bottom of the page)
+    languages = languages.slice(0, -1)
+  })
+  .catch(err => console.log(err))
 
 // TODO https://www.npmjs.com/package/twitter
-var client = new Twitter({
+/* const client = new Twitter({
   consumer_key: config.get('twitter.consumerKey'),
   consumer_secret: config.get('twitter.consumerSecret'),
   access_token_key: config.get('twitter.accessTokenKey'),
   access_token_secret: config.get('twitter.accessTokenSecret')
 })
-
-// TODO set up logging to console depending on verbosity
-
-// TODO interact with cache
-
-const wikipediasTableURL = 'https://meta.wikimedia.org/wiki/List_of_Wikipedias/Table'
-const wikipediaStatsURL = subdomain => 'https://' + subdomain + '.wikipedia.org/wiki/Special:Statistics'
-
-// TODO okay, actually make the thing
-
-
-const getWikipedias = () => {
-  axios.get(wikipediasTableURL)
-    .then(src => {
-      console.log(src.data)
-      const dom = new JSDOM(src.data)
-      //console.log(dom.window.document)
-      // TODO this just doesn't parse out anything at all? but it doesn't yield an error, either.
-      const rows = dom.window.document.querySelectorAll('.mw-parser-output table tbody tr td:nth-child(2n):nth-child(-n+4)')
-      //console.log(rows)
-
-    })
-    .catch(err => console.log(err))
-}
-
-//getWikipedias()
-
-const cheerio = require('cheerio')
-
-axios.get('https://meta.wikimedia.org/wiki/List_of_Wikipedias/Table')
-  .then(src => {
-    //console.log(src.data)
-    const $ = cheerio.load(src.data)
-    //let test = $('.mw-parser-output table tbody tr td:nth-child(2n):nth-child(-n+4)').text()
-    //console.log(test.split("\n"))
-    let rows = $('.mw-parser-output table tr')
-    //let rows2 = rows.map((i, row) => {
-    //  return row.children.filter((i, e) => {this.type === 'td'})
-    //});
-    //console.log(rows2);
-    for (var i = 0; i < rows.length; i++) {
-      // need to first filter out theads somehow, ugh
-      if (rows[i].children[3].children[0].type == "tag") {
-        let language = rows[i].children[3].children[0].children[0].data
-        let lang = rows[i].children[7].children[0].children[0].data
-        // TODO $($(rows[i]).find("td")[1]).text()
-        // TODO $($(rows[i]).find("td")[3]).text()
-        // TODO rows.slice(0, -1).map((i,e) => [$($(e).find("td")[1]).text(), $($(e).find("td")[3]).text()])
-        console.log([language, lang])
-      }
-      //console.log(rows.find("td"))
-      //console.log(rows[i].children[1], rows[i].children[3])
-      //console.log(rows[i].children.filter((i, e) => {console.log(e);return true || e.type == 'text'}))
-    }
-    // TOOD okay, can do things with this.
-  })
-  .catch(err => console.log(err))
-
-
-/*axios.get('https://wikipedia.org')
-  .then(src => {
-    console.log(src.data)
-    const dom = new JSDOM(src.data)
-    console.log(dom)
-    //console.log(Array.from(dom.window.document.querySelectorAll('li')).map(node => node.textContent))
-    //dom.window.document.querySelectorAll('li').forEach(node => console.log(node.textContent))
-  })
-  .catch(err => console.log(err))*/
-
-/* client.post('statuses/update', { status: 'testing' })
-  .then(function (tweet) {
+client.post('statuses/update', { status: 'testing' })
+  .then(tweet => {
     console.log(tweet)
   })
-  .catch(function (error) {
+  .catch(error => {
     throw error
   }) */
 
@@ -106,3 +103,22 @@ axios.get('https://meta.wikimedia.org/wiki/List_of_Wikipedias/Table')
 
 // TODO dict intersection: list1.filter(a => list2.some(b => a.userId === b.userId));
 // via https://stackoverflow.com/a/54763194
+
+async function run() {
+  //const wikipedias = await asyncTask().catch(error => console.error(error));
+
+
+
+
+  // TODO
+  /*
+  load wikipedias list
+  then
+  for each wikipedia, load it and extract stats
+  then
+  load cache or, if not possible, make empty cache/skip tweeting
+  then
+  compare and construct tweets and send em
+  then
+  write new cache to disk
+}
