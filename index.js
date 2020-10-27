9518742c5fd19ccc92de709ab104b0c75a14fcfe
Noah Doersing <git@noahdoersing.com>
2020-10-27 14:46:40 +0100

    Fix typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const bot = require('./src/bot.js')
 
-// Unless the NODE_CONFIG_DIR environment variable is set, the automatically
+// Unless the NODE_CONFIG_DIR environment variable is set, this automatically
 // loads "config/default.yaml" if existant in the current working directory.
 const config = require('config')
 
